9e1cdeaf5cf47783bfc1b4772fea73cc80e512f1
Joe Simoes <joe@generalassemb.ly>
2015-01-13 22:29:19 -0500

    serialize cart data

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,8 +1,15 @@
 class Cart
   attr_reader :items
 
-  def initialize
-    @items = []
+  def self.build_from_hash hash 
+    items = hash['items']['cart'].map do |item_data|
+      CartItem.new item_data['product_id'], item_data['quantity']
+    end
+    new items
+  end
+
+  def initialize items = []
+    @items = items
   end
 
   def add_item product_id
@@ -16,5 +23,20 @@ class Cart
 
   def empty?
     @items.empty?
+  end
+
+  def serialize
+    items = @items.map do |item| 
+      { 
+        "product_id" => product.id, 
+        "quantity" => item.quantity
+      }
+    end
+
+    {
+      "cart" => {
+        "items" => items
+      }
+    }
   end
 end
